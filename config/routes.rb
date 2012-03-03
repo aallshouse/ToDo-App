c1321b61209d33fe76fbf0f40fe50f0680d6d7b9
user <user@ubuntu1010desktop.(none)>
2012-03-02 23:18:56 -0500

    big honking commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TodoApp::Application.routes.draw do
+  resources :tasks
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- features/step_definitions/todo_steps.rb
+++ features/step_definitions/todo_steps.rb
@@ -6,7 +6,7 @@ When /^I create a task item named "([^"]*)"$/ do |title|
   fill_in "Title", :with => title
 end
 
-Then /^I see "([^"]*)" in my list of task items$/ do |arg1|
-  page.has_content?("Task created successfully")
+Then /^I see "([^"]*)" in my list of task items$/ do |text|
+  page.has_content?(text).should be_true
 end
 
