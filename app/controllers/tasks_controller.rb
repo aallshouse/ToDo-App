7d4644381de9f4e554d2b8d6b01ee10d7f3f82df
user <user@ubuntu1010desktop.(none)>
2012-03-06 22:51:08 -0500

    Add create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,11 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def index
+  end
+
+  def create
+    @task = Task.new(params[:task])
+  end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -7,4 +7,11 @@ describe TasksController do
       response.code.should eq('200')
     end
   end
+
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index
+      response.code.should eq('200')
+    end
+  end
 end
