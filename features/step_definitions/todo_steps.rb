a98d82d72bd0e311b7dd48c917baf35d37147b1a
user <user@ubuntu1010desktop.(none)>
2012-03-06 20:52:16 -0500

    added test for task controller new action

--- features/step_definitions/todo_steps.rb
+++ features/step_definitions/todo_steps.rb
@@ -9,7 +9,3 @@ end
 Then /^I see "([^"]*)" in my list of task items$/ do |text|
   page.has_content?(text).should be_true
 end
-<<<<<<< HEAD
-
-=======
->>>>>>> 9cc6f5aa332107233855e930641ef95b4a75458c
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe TasksController do
-
+  describe "GET new" do
+    it "has a 200 status code" do
+      get :new
+      response.code.should eq('200')
+    end
+  end
 end
